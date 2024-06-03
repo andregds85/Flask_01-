5cd86f7b296fe1f9c1811660c025ee2d7ba92c6b
André <andregds85@gmail.com>
2024-06-03 04:21:22 -0300

    Deploy dos arquivos no Github

---
+++ anotação.php
@@ -0,0 +1,38 @@
+# criando uma aplicação 
+https://www.freecodecamp.org/portuguese/news/como-criar-uma-aplicacao-da-web-usando-o-flask-e-implanta-la-na-nuvem/
+
+# Hopedagem em Python 
+https://www.youtube.com/watch?v=n_9FQVcqf4I 
+
+
+# Instalação do Flask 
+https://www.alura.com.br/artigos/flask-instalacao
+
+# Rodar o Servidor
+python main.py 
+
+# Comandos GIT 
+git config --global user.name "André"
+git config --global user.email andregds85@gmail.com
+
+# ...
+git add --all
+git commit -m "Deploy dos arquivos no Github"
+
+# ...
+git push -u origin master
+git push --force origin main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
